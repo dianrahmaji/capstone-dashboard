1abeaffb108e4f1ea81c9ac96e0591698065ca96
Dian Rahmaji <dianrahmaji@gmail.com>
2022-06-09 11:11:18 +0700

    KMND-52 Added Chat Bubble UI (#56)
    * added added ChatBubble and ChatContainer UI
    
    * added user props to ChatBubble
    
    * added loggedInId checking for ChatBubble position
    
    * added loggedInId checking for ChatBubble colors
    
    * added scroll to MessageEnd

--- src/pages/ResearchDiscussion/components/ChatBubble.jsx
+++ src/pages/ResearchDiscussion/components/ChatBubble.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import clsx from 'clsx'
 
-const ChatBubble = () => {
-  return <div>ChatBubble</div>
+const loggedInId = 1
+
+const ChatBubble = ({ user }) => {
+  return (
+    <div
+      className={clsx('flex', {
+        'justify-end': user._id === loggedInId
+      })}
+    >
+      <div className="text-black w-fit max-w-xl">
+        {user._id !== loggedInId && <p className="text-sm">{user.fullName}</p>}
+        <p
+          className={clsx(' p-4 mb-3 rounded-xl', {
+            'bg-gray-200': user._id !== loggedInId,
+            'bg-primary text-white': user._id === loggedInId
+          })}
+        >
+          ChatBubblesadasdasdasdasd
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default ChatBubble
--- src/pages/ResearchDiscussion/components/ChatContainer.jsx
+++ src/pages/ResearchDiscussion/components/ChatContainer.jsx
@@ -1,12 +1,32 @@
+import { useEffect, useRef } from 'react'
+
 import ChatBubble from './ChatBubble'
 
+const user1 = {
+  _id: 1,
+  fullName: 'Dian Rahmaji'
+}
+
+const user2 = {
+  id: 2,
+  fullName: 'Dzakiy Harissalam'
+}
+
 const ChatContainer = () => {
+  const endMessage = useRef(null)
+
+  useEffect(() => {
+    endMessage.current?.scrollIntoView()
+  })
+
   return (
     <div className="w-full mx-auto px-4 sm:px-6 md:px-14 overflow-y-scroll h-7/8">
-      <div className="flex flex-col justify-end">
-        {[...Array(120)].map((_, i) => (
-          <ChatBubble key={i} />
+      <div className="flex flex-col">
+        {[...Array(110)].map((_, i) => (
+          <ChatBubble key={i} user={user2} />
         ))}
+        <ChatBubble user={user1} />
+        <div ref={endMessage} />
       </div>
     </div>
   )
