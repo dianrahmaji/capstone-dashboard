0eaff77b163f6f5978699acebee17d0c90fe984a
Dian Rahmaji <dianrahmaji@gmail.com>
2022-06-14 08:46:26 +0700

    KMND-59 Setup Vitest (#63)
    * added test modules
    
    * added Login page test file
    
    * added coverage to .gitignore
    
    * added test config

---
+++ src/pages/__test__/Login.test.jsx
@@ -0,0 +1,30 @@
+import { describe, expect, it } from 'vitest'
+import { render, screen } from '@testing-library/react'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
+
+import Login from '../Login'
+
+describe('Login', () => {
+  const initialState = {}
+  const mockStore = configureStore()
+  let store
+
+  it('should render page', () => {
+    store = mockStore(initialState)
+
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    )
+
+    expect(screen.getByText(/Sign in to your account/i)).toBeDefined()
+  })
+})
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom'
+  },
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`
