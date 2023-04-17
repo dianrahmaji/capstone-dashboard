34ca886a25293435c1a231db27e55b9fb353d5a3
Dian Rahmaji <dianrahmaji@gmail.com>
2023-04-17 22:45:43 +0700

    feat: update HistoryModal UI (#253)

--- src/pages/ResearchDocumentation/components/document/HistoryModal.jsx
+++ src/pages/ResearchDocumentation/components/document/HistoryModal.jsx
@@ -11,7 +11,7 @@ const activity = [
     imageUrl:
       "https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
     comment:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt nunc ipsum tempor purus vitae id. Morbi in vestibulum nec varius. Et diam cursus quis sed purus nam. ",
+      "Perbaikan tata tulis, penambahan saran mengenai aspek pengamanan, fitur dokumen/link eksternal, penjadwalan proyek dan analisis S-curve, menambah implementasi wiki, referensi awal dokumentasi penelitian, evaluasi dokumentasi penelitian danriwayat dokumentasi",
     date: "1h ago",
   },
   {
@@ -21,9 +21,18 @@ const activity = [
     imageUrl:
       "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
     comment:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt nunc ipsum tempor purus vitae id. Morbi in vestibulum nec varius. Et diam cursus quis sed purus nam. Scelerisque amet elit non sit ut tincidunt condimentum. Nisl ultrices eu venenatis diam.",
-    date: "2h ago",
+      "Perbaikan pengantar, perbaikan penjelasan analisis dan pemilihan metode, penambahan penjelasan mengenai pengujian Google Lighthouse pada aspek accessibility",
+    date: "4d ago",
   },
+  // {
+  //   id: 3,
+  //   person: { name: "Dian Rahmaji", href: "#" },
+  //   version: "1.0.0",
+  //   imageUrl:
+  //     "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
+  //   comment: "Penyusunan Dokumen C-501",
+  //   date: "12d ago",
+  // },
 ];
 
 export default function HistoryModal({
@@ -36,15 +45,14 @@ export default function HistoryModal({
     <BaseModal title={`Riwayat ${item.name}.${item.extension}`} {...props}>
       <div className="mt-8 flow-root">
         <ul className="-mb-8">
-          {activity.map((activityItem, activityItemIdx) => (
+          {activity.map((activityItem) => (
             <li key={activityItem.id}>
               <div className="relative pb-8">
-                {activityItemIdx !== activity.length - 1 ? (
-                  <span
-                    className="absolute left-5 top-5 -ml-px h-full w-0.5 bg-gray-200"
-                    aria-hidden="true"
-                  />
-                ) : null}
+                <span
+                  className="absolute left-5 top-5 -ml-px h-full w-0.5 bg-gray-200"
+                  aria-hidden="true"
+                />
+
                 <div className="relative flex items-start space-x-3">
                   <div className="relative">
                     <div className="my-auto flex h-12 w-12 items-center justify-center rounded-full bg-blue-100">
@@ -82,6 +90,45 @@ export default function HistoryModal({
               </div>
             </li>
           ))}
+          <li>
+            <div className="relative pb-8">
+              {/* <span
+                className="absolute left-5 top-5 -ml-px h-full w-0.5 bg-gray-200"
+                aria-hidden="true"
+              /> */}
+
+              <div className="relative flex items-start space-x-3">
+                <div className="relative">
+                  <div className="my-auto flex h-12 w-12 items-center justify-center rounded-full bg-blue-100">
+                    <div className=" text-sm text-primary">
+                      {getProfileFromFullName("Dian Rahmaji")}
+                    </div>
+                  </div>
+                </div>
+                <div className="min-w-0 flex-1">
+                  <div>
+                    <div className="text-sm">
+                      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                      <a href="#" className="font-medium text-gray-900">
+                        Dian Rahmaji
+                      </a>{" "}
+                      mengunggah dokumen versi{" "}
+                      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                      <a href="#" className="font-medium text-gray-900">
+                        1.0.0
+                      </a>
+                    </div>
+                    <p className="mt-0.5 text-sm text-gray-500">
+                      Memberikan komentar 12d ago
+                    </p>
+                  </div>
+                  <div className="mt-2 text-sm text-gray-700">
+                    <p>Penyusunan Dokumen C-501</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
         </ul>
         <div className="mt-4 flex gap-4">
           <BaseButton
